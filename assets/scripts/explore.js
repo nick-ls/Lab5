717c4a42612b367ea0fdf5cd0d8d22e224932db7
Nick <nick-ls@users.noreply.github.com>
2023-05-07 02:36:04 -0700

    finshfinsh site

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,28 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+	const voiceSelect = document.getElementById("voice-select");
+	const talkBtn = document.querySelector("#explore > button");
+	const face = document.querySelector("#explore > img");
+	const tts = document.getElementById("text-to-speak");
+
+	let voices = speechSynthesis.getVoices();
+	for (let voice of voices) {
+		let opt = document.createElement("option");
+		opt.value = voice.name;
+		opt.textContent = voice.name;
+		voiceSelect.appendChild(opt);
+	}
+	
+	talkBtn.addEventListener("click", () => {
+		let utter = new SpeechSynthesisUtterance(tts.value);
+		utter.voice = voices.find(x=>x.name === voiceSelect.value);
+		speechSynthesis.speak(utter);
+	});
+
+	const makeTalk = () => {
+		face.src = `./assets/images/smiling${speechSynthesis.speaking ? "-open" : "" }.png`;
+		requestAnimationFrame(makeTalk);
+	}
+	makeTalk();
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,43 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+	const confetti = new JSConfetti();
+
+	let hornSelect = document.getElementById("horn-select");
+	let hornImg = document.querySelector("#expose > img");
+	let volumeControl = document.querySelector("#volume");
+	let volumeIcon = document.querySelector("#volume-controls > img");
+	let playSound = document.querySelector("#expose > button");
+	let audioSource = document.querySelector("audio");
+
+	const syncVolume = () => {
+		let v = volumeControl.value;
+		let volumeLevel = v == 0 ? 0 :
+			v < 33 ? 1 :
+			v < 66 ? 2 : 3;
+
+		audioSource.volume = v / 100;
+
+		volumeIcon.src = `./assets/icons/volume-level-${volumeLevel}.svg`;
+		volumeIcon.alt = `Volume level ${volumeLevel}`
+	};
+
+	const syncSFX = () => {
+		let h = hornSelect.value;
+		if (h === "select") return;
+		hornImg.src = `./assets/images/${h}.svg`;
+		audioSource.src = `./assets/audio/${h}.mp3`;
+	};
+
+	playSound.addEventListener("click", () => {
+		audioSource.currentTime = 0;
+		audioSource.play();
+		if (hornSelect.value === "party-horn") confetti.addConfetti();
+	});
+
+	volumeControl.addEventListener("input", syncVolume);
+	hornSelect.addEventListener("change", syncSFX);
+
+	syncSFX();
+	syncVolume();
 }
